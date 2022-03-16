9598e384331bb758ab016c1b2cbcc536627fc364
Mariana Alvares <mariana11areal@hotmail.com>
2022-03-16 18:08:28 -0300

    Visual do Dashboard em construção

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,13 @@
 import React from "react";
-import {Box, Toolbar} from "@mui/material"
+import {
+  Box,
+  Toolbar,
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { Add, Remove } from "@mui/icons-material";
 
 function Dashboard() {
   return (
@@ -13,7 +21,99 @@ function Dashboard() {
       }}
     >
       <Toolbar />
-      <div>Dashboard</div>
+      <Grid container spacing={3}>
+        <Grid item lg={12} md={12}>
+          <Typography variant="h5" sx={{ fontWeight: "700", color: "#4e4f50" }}>
+            Dashboard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Aqui você encontrará informações sobre suas transações.
+          </Typography>
+        </Grid>
+        <Grid item lg={4} md={6}>
+          <Card variant="outlined">
+            <CardContent>
+              <Grid container alignItems="center">
+                <Grid item>
+                  <Add
+                    style={{
+                      backgroundColor: "#5c8c38",
+                      fill: "#fff",
+                      borderRadius: "50%",
+                      padding: "4px",
+                      marginRight: "20px",
+                      boxShadow: "2px 5px 9px -3px #000000a6",
+                    }}
+                  />
+                </Grid>
+                <Grid item>
+                  <Typography variant="h5">12</Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Transações de Entrada
+                  </Typography>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item lg={4} md={6}>
+          <Card variant="outlined">
+            <CardContent>
+            <Grid container alignItems="center">
+                <Grid item>
+                  <Remove
+                    style={{
+                      backgroundColor: "#e42629",
+                      fill: "#fff",
+                      borderRadius: "50%",
+                      padding: "4px",
+                      marginRight: "20px",
+                      boxShadow: "2px 5px 9px -3px #000000a6",
+                    }}
+                  />
+                </Grid>
+                <Grid item>
+                <Typography variant="h5">5</Typography>
+              <Typography variant="body2" color="text.secondary">
+                Transações de Saída
+              </Typography>
+                </Grid>
+              </Grid>
+              
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item lg={4} md={6}>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography variant="h5">R$ 2.000,00</Typography>
+              <Typography variant="body2" color="text.secondary">
+                Subtotal de Entrada
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item lg={4} md={6}>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography variant="h5">R$ 2.000,00</Typography>
+              <Typography variant="body2" color="text.secondary">
+                Subtotal de Saída
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item lg={4} md={6}>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography variant="h5">R$ 2.000,00</Typography>
+              <Typography variant="body2" color="text.secondary">
+                Subtotal da Conta
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
